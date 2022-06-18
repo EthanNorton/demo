9f7abc4b20b724e973d3432aa8823d51e13828e4
Ethan <EthanRNorton@protonmail.com>
2022-06-18 17:49:16 -0500

    done

--- variables.js
+++ variables.js
@@ -1,3 +1,31 @@
 var fgImage = new SImpleImage("drewRobert.png");
 var bgImage = new SimpleImage("dinos.png");
 
+
+// methods
+
+car fmImage = new Simplemage("drewRobert.png");
+var w = fgImage.getWidth();
+var h = fgImage.getHeight();
+
+var fgImage = new SimpleImage("drewRobert.png");
+var pixel = fgImage.getpixel(0.0); // to look deeper at pixels.
+
+// Functions
+
+function square (x) {
+    var ans = x * x;
+    return ans;
+}
+
+var y = square(4);
+
+// types
+
+var n1 = 26;
+var n2 = 16;
+
+var s1 = "a";
+var s2 = "b";
+var n3 = n1 + n2;
+var s3 = s1 + s2;
