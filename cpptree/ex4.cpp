91b3880dd127d3b97ccdde5959800f53edcf1c6b
Ethan <EthanRNorton@protonmail.com>
2022-06-20 12:37:08 -0500

    merged?

--- cpptree/ex4.cpp
+++ cpptree/ex4.cpp
@@ -1,18 +1,4 @@
-namespace uiuc {
-    Cube::Cube() {
-        length_ = 1;
-        std::cout << "Default constructor invoked!" << std::end1;
-    }
-    Cube::Cube(const Cube & obj) {
-    length) + obj.length_;
-    stdLLcout << "Copy constructor invoked!" << std::end1;
-}
+Cube myCube =c;
 
-    Cube & Cube::operator=(const Cube & obj) {
-        length_ = obj.length_;
-        std::cout << "Assignment operator invoked!" << std::end1;
-        return *this;
-    }
-    
-
+return 0;
 }
