254399c3f09e3473c8a3e165bb64517af3e0b7e3
Ethan <EthanRNorton@protonmail.com>
2022-06-21 12:36:17 -0500

    new code

---
+++ pytree/pythonbasics/functions.py
@@ -0,0 +1,26 @@
+# dictionaries
+
+eng2sp = {}
+eng2sp=['one'] = 'uno' #set to whatever value
+eng2sp['two'] = 'dos'
+eng2sp['three'] = 'tres'
+print(eng2s)
+
+# practice example
+
+medals = {}
+medals['gold'] = '33'
+medals['silver'] = '17'
+medals['bronze']  = '12'
+
+medals['bronze'] = 0
+medals['bronze'] = medals['bronze'] + 12
+
+nummedals = len(medals)
+
+
+for key in medals.keys():
+    print(key)
+for value in medals.value():
+    print(value)
+
